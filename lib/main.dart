8e9353aaf76fe777aeba8f5b63ed467b188589ec
vikepk <prempk0516@gmail.com>
2023-04-18 22:53:16 +0530

    Firebase Config added need to test

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,25 @@ import 'package:out_of_gas/services/map.dart';
 import 'package:out_of_gas/pages/first_page.dart';
 import 'package:out_of_gas/pages/helper_page.dart';
 import 'package:out_of_gas/pages/need_gas.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() => runApp(MaterialApp(
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    initialRoute: '/first',
+    routes: {
+      '/map': (context) => Map(),
+      '/helper': (context) => Helper(),
+      '/first': (context) => HomePage(),
+      '/location': (context) => Currentlocation(),
+      '/need_gas': (context) => Need_Gas(),
+    },
+  ));
+}
+
+/*void main() => runApp(MaterialApp(
       debugShowCheckedModeBanner: false,
       initialRoute: '/first',
       routes: {
@@ -20,4 +37,4 @@ void main() => runApp(MaterialApp(
         '/location': (context) => Currentlocation(),
         '/need_gas': (context) => Need_Gas(),
       },
-    ));
+    ));*/
