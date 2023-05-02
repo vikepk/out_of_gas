e295985c371b97a9b993560dc3565328556dea6f
vikepk <prempk0516@gmail.com>
2023-05-02 19:31:49 +0530

    Firebase data acessing ways checking

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:out_of_gas/pages/need_gas.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:out_of_gas/pages/helper_page.dart';
 import 'package:out_of_gas/pages/wait.dart';
+//import 'package:out_of_gas/pages/tried.dart';
 
 //import 'package:out_of_gas/firebase_options.dart';
 void main() async {
@@ -24,6 +25,7 @@ void main() async {
       '/need_gas': (context) => Need_Gas(),
       '/read_data': (context) => ReadData(),
       '/wait': (context) => Wait(),
+      //'/tried': (context) => Helper(),
     },
   ));
 }
